1c37139b63f018b7ae85aa05eb2513ea5fd5bd0e
Annaleigh Thomas <annaleighxvx@gmail.com>
2021-04-26 17:25:54 -0400

    header rendered

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Component } from 'react';
 import logo from './react-logo.svg';
 import './App.css';
 import React from 'react';
+import Header from './Header';
 
 class App extends Component {
 
@@ -9,10 +10,9 @@ class App extends Component {
     return (
       <div className="App">
   
-        My React App...
+        <Header/>
         
-        <img src={logo} className="temp-images" alt="react logo" />
-        <img src="acl-logo.png" className="temp-images" alt="acl logo" />
+
       
       </div>
     );
---
+++ src/Header.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './Header.css';
+
+class Header extends Component {
+
+  render() {
+    return (
+      <header className="Header">
+
+        <img className="logo" alt="creature logo" src=""/>
+
+        <h1>Horned Creatures</h1>
+
+      </header>
+    );
+  }
+}
+
+
+export default Header;
