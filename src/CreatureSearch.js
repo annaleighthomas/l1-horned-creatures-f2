cc2172812cf0479d7f0ce64cbdc4a85555ec033b
Annaleigh Thomas <annaleighxvx@gmail.com>
2021-04-27 17:06:38 -0400

    added input field and select

--- src/CreatureSearch.js
+++ src/CreatureSearch.js
@@ -7,6 +7,14 @@ export default class CreatureSearch extends Component {
     return (
       <form className="CreatureSearch">
 
+        <input placeholder="horned creatures"/>
+
+        <select>
+          <option value="">sort...</option>
+          <option value="title">by name</option>
+          <option value="horns">by horns</option>
+        </select>
+
         <button>Search</button>
 
       </form>
